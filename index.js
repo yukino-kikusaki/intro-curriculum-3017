788573d4fb87d5512938f1cecf28e960da7d0815
taigaozawa <taigaozawa@gmail.com>
2021-03-28 20:21:52 +0900

    Dockerでデプロイできるように変更

--- index.js
+++ index.js
@@ -6,74 +6,82 @@ const basic = auth.basic(
   { realm: 'Enquetes Area.' },
   (username, password, callback) => {
     callback(username === 'guest' && password === 'xaXZJQmE');
-  }
-);
-const server = http
-  .createServer(basic, (req, res) => {
-    console.info('Requested by ' + req.connection.remoteAddress);
+  });
+const server = http.createServer(basic, (req, res) => {
+  console.info('Requested by ' + req.socket.remoteAddress);
 
-    if (req.url === '/logout') {
-      res.writeHead(401, {
-        'Content-Type': 'text/plain; charset=utf-8'
-      });
-      res.end('ログアウトしました');
-      return;
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
+  if (req.url === '/logout') {
+    res.writeHead(401, {
+      'Content-Type': 'text/plain; charset=utf-8'
     });
+    res.end('ログアウトしました');
+    return;
+  }
 
-    switch (req.method) {
-      case 'GET':
-        if (req.url === '/enquetes/yaki-shabu') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '焼き肉',
-              secondItem: 'しゃぶしゃぶ'
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
-            })
-          );
-        } else if (req.url === '/enquetes/sushi-pizza') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '寿司',
-              secondItem: 'ピザ'
-            })
-          );
-        }
-        res.end();
-        break;
-      case 'POST':
-        let rawData = '';
-        req
-          .on('data', chunk => {
-            rawData = rawData + chunk;
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  });
+
+  switch (req.method) {
+    case 'GET':
+      if (req.url === '/') {
+        res.write('<!DOCTYPE html><html lang="ja"><body>' +
+          '<h1>アンケートフォーム</h1>' +
+          '<a href="/enquetes">アンケート一覧</a>' +
+          '</body></html>');
+      } else if (req.url === '/enquetes') {
+        res.write('<!DOCTYPE html><html lang="ja"><body>' +
+          '<h1>アンケート一覧</h1><ul>' +
+          '<li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
+          '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+          '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+          '</ul></body></html>');
+      } else if (req.url === '/enquetes/yaki-shabu') {
+        res.write(
+          pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: '焼き肉',
+            secondItem: 'しゃぶしゃぶ'
           })
-          .on('end', () => {
-            const qs = require('querystring');
-            const answer = qs.parse(rawData);
-            const body = answer['name'] + 'さんは' +
-              answer['favorite'] + 'に投票しました';
-            console.info(body);
-            res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-              body + '</h1></body></html>');
-            res.end();
-          });
-        break;
-      default:
-        break;
-    }
-  })
+        );
+      } else if (req.url === '/enquetes/rice-bread') {
+        res.write(
+          pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: 'ごはん',
+            secondItem: 'パン'
+          })
+        );
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '寿司',
+          secondItem: 'ピザ'
+        }));
+      }
+      res.end();
+      break;
+    case 'POST':
+      let rawData = '';
+      req
+        .on('data', chunk => {
+          rawData = rawData + chunk;
+        })
+        .on('end', () => {
+          const qs = require('querystring');
+          const answer = qs.parse(rawData);
+          const body = answer['name'] + 'さんは' +
+            answer['favorite'] + 'に投票しました';
+          console.info(body);
+          res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+            body + '</h1></body></html>');
+          res.end();
+        });
+      break;
+    default:
+      break;
+  }
+})
   .on('error', e => {
     console.error('Server Error', e);
   })
